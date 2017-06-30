4b572a6ceab4703bdd30f79ec7ef3f8a2bf4bd7a
Maryna Kontar <marina.acoustic@gmail.com>
2017-06-30 10:36:08 +0300

    commit App

--- src/com/App.java
+++ src/com/App.java
@@ -22,7 +22,43 @@ public class App {
         em.persist(p);
 //        dao.save(p);
 
-    em.getTransaction().commit();//!!!
+//        Person p1=em.find(Person.class, 1);
+//        System.out.println(p1);
+//
+//        Employee emp = new Employee();
+//        emp.setName("Employee");
+//        emp.setAge(22);
+//        emp.setEmployeeType(EmployeeType.A);
+//
+//        Department department = new Department();
+//        department.setTitle("T1");
+//        department.setYear(1234);
+//
+//        Animal animal = new Animal();
+//        animal.setName("A");
+//        animal.setAge(1);
+//        animal.setNameOfAnimal("NameA");
+//        em.persist(animal);
+//
+//        /*Animal animal1 = new Animal();
+//        animal1.setName("A");
+//        animal1.setAge(1);
+//        animal1.setNameOfAnimal("NameB");*/
+//
+//
+//        Car car = new Car();
+//        car.setManufactory("A");
+//        CarId carId = new CarId();
+//        carId.setAge(10);
+//        carId.setModel("M1");
+//
+//        em.persist(car);
+//
+//
+//        em.persist(emp);
+//
+//
+//    em.getTransaction().commit();//!!!
 
     }
 }
